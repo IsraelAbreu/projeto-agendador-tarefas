cb7ef4876c09857f0b76e4909844421208a00745
IsraelAbreu <israeloliveira18@hotmail.com>
2026-02-20 23:06:04 -0300

    feat: definidos Entitys:Usuario, Telefones, Endereços + SpringSecurity

---
+++ src/main/java/com/javanauta/usuario/infraestructure/entity/Usuario.java
@@ -0,0 +1,63 @@
+package com.javanauta.usuario.infraestructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "usuarios")
+
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", length = 100)
+    private String nome;
+    @Column(name = "email", length = 100)
+    private String email;
+    @Column(name = "senha")
+    private String senha;
+
+    // Relacionamento 1 usuario para N endereços
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+
+    private List<Enderecos> enderecos;
+
+    // Relacionamento 1 usuario para N números
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+
+    private List<Telefones> telefones;
+
+    //Métodos da Interface UserDetails - SpringSecurity
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
